824739c7750f597661bdaf4e5c8efd14e3559402
Jeremiah Anthony Nuqui <jmnuqui@sjcgroup.ph>
2019-11-25 17:18:30 +0800

    Add server

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const fs = require('fs');
 const path  = require('path');
 const os = require('os');
 const url = require('url');
+const EventEmitter = require('events');
+const http = require('http');
 
 //--create file
 // fs.writeFile(
@@ -62,15 +64,67 @@ const url = require('url');
 
 
 //---URL Module
-const myUrl = new URL('http://mywebsite.com/hello.html?id=123&status=active');
+// const myUrl = new URL('http://mywebsite.com/hello.html?id=123&status=active');
 
-console.log('href: -------------' + myUrl.href);
-console.log('to string: --------' + myUrl.toString());
-console.log('host: -------------' + myUrl.host);
-console.log('hostname: ---------' + myUrl.hostname);
-console.log('pathname: ---------' + myUrl.pathname);
-console.log('search: -----------' + myUrl.search);
-console.log('searchParams: -----' + myUrl.searchParams);
+// console.log('href: -------------' + myUrl.href);
+// console.log('to string: --------' + myUrl.toString());
+// console.log('host: -------------' + myUrl.host);
+// console.log('hostname: ---------' + myUrl.hostname);
+// console.log('pathname: ---------' + myUrl.pathname);
+// console.log('search: -----------' + myUrl.search);
+// console.log('searchParams: -----' + myUrl.searchParams);
 
-myUrl.searchParams.append('abc', '123');
-console.log('append: -----------' + myUrl.searchParams);
+// myUrl.searchParams.append('abc', '123');
+// console.log('append: -----------' + myUrl.searchParams);
+// myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`))
+
+
+
+// //Create Class
+// class MyEmitter extends EventEmitter {
+//
+// }
+// //Init Object
+// const myEmitter = new MyEmitter();
+// // Event Listener
+// myEmitter.on('event', () => console.log('Event Fired!!!'));
+// myEmitter.emit('event');
+// myEmitter.emit('event');
+// myEmitter.emit('event');
+// myEmitter.emit('event');
+
+// //Logger With Event Emitter
+// const uuid = require('uuid');
+//
+// class Logger extends EventEmitter {
+//   log(msg) {
+//     //call event
+//     this.emit('message', {id: uuid.v1(), msg });
+//   }
+// }
+//
+// const logger = new Logger();
+// logger.on('message', (data) => console.log('Called Listener:', data));
+// logger.log('Hello World');
+
+
+// Http Module
+// http.createServer((req, res) => {
+//   res.write('Hello World');
+//   // res.end();
+//   console.log("current url: ", req.url);
+//   // res.end();
+// }).listen(5000, () => console.log('Server running...'));
+
+
+// //Create a Server
+let server = http.createServer((req, res) => {
+  if(req.url == "/"){
+    res.write('<h1>Home</h1>');
+  } else if(req.url == "/about") {
+    res.write('<h1>About</h1>');
+  }
+  console.log("current url: " + req.url);
+  res.end();
+});
+server.listen(5000, () => console.log("zxc"));
